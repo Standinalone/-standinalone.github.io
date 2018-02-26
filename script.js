7fac238ead109a8b7ddf54ca01408f4d4934e0c2
Standinalone <36714936+Standinalone@users.noreply.github.com>
2018-02-26 18:08:08 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+	function buttonClick(str)
+	{
+		var btn = document.createElement('button');
+		btn.innerHTML = 'X';
+		btn.addEventListener ('click', function(){this.parentNode.parentNode.parentNode.removeChild(this.parentNode.parentNode)}); // 100% можно это по другому сделать.. или нет?
+		
+		if (str){
+			var task = str;
+		}
+		else{
+			var task = document.getElementById('input').value;
+		}
+		var table = document.getElementById('myTable');
+
+		td1=document.createElement('td');
+		td2=document.createElement('td');
+		tr=document.createElement('tr')
+		
+		tr.appendChild(td2);
+		tr.appendChild(td1);
+
+		td1.appendChild(btn);
+		td2.innerHTML=task;
+		table.appendChild(tr);
+	}
